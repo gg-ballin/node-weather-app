cb6049bcc55e7ea6727fa9889a6643d9b8929625
Lucas Coria <l.coria@paisanoscreando.com>
2019-11-28 10:12:15 -0300

    Nuevos cambios

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /* eslint-disable indent */
 const request = require('request');
-
+// Seccion 6-35 The callback function
 // const url = 'https://api.darksky.net/forecast/f00eca571fb141085313ed93d4ec2518/37.8267,-122.4233';
 
 // request({url: url, json: true}, (error, response) => {
